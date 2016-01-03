3a8ed4f04b44267f4e2dba0302e5dc599b65e1e8
Tyler Bonnell <tbonnell@uw.edu>
2016-01-02 23:59:12 -0800

    Initial commit
    made a lil robot who can punch

---
+++ Assets/Arm.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arm : MonoBehaviour {
+
+	public Rigidbody forearm;
+	public KeyCode punchKey;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (punchKey)) {
+			Debug.Log ("punch!");
+			forearm.AddForce (Vector3.right * 3000 + Vector3.up * 2000, ForceMode.Acceleration);
+		}
+	}
+}
