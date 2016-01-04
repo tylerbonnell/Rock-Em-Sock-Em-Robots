c0ae816929d34cf1c3f4af433e9d14fdc4346440
Tyler Bonnell <tbonnell@uw.edu>
2016-01-03 18:14:11 -0800

    Two players working well
    going to add scoring/death next

--- Assets/Arm.cs
+++ Assets/Arm.cs
@@ -3,19 +3,35 @@ using System.Collections;
 
 public class Arm : MonoBehaviour {
 
-	public Rigidbody forearm;
+	public HingeJoint upperArm;
+	public HingeJoint forearm;
+	private JointSpring upperArmSpring;
+	private JointSpring forearmSpring;
 	public KeyCode punchKey;
-
-	// Use this for initialization
+	private float initialUpperPos;
+	private float initialLowerPos;
+	
 	void Start () {
-		
+		initialUpperPos = upperArm.spring.targetPosition;
+		upperArmSpring = upperArm.spring;
+		initialLowerPos = forearm.spring.targetPosition;
+		forearmSpring = forearm.spring;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (punchKey)) {
-			Debug.Log ("punch!");
-			forearm.AddForce (Vector3.right * 3000 + Vector3.up * 2000, ForceMode.Acceleration);
+			//forearm.AddForce (Vector3.right * 30 * direction + Vector3.up * 15, ForceMode.Impulse);
+			setTargetPositions (-20f, 0f);
+		} else if (Input.GetKeyUp (punchKey)) {
+			setTargetPositions (initialUpperPos, initialLowerPos);
 		}
+	}
+	
+	private void setTargetPositions (float upperArmTarget, float forearmTarget) {
+		upperArmSpring.targetPosition = upperArmTarget;
+		forearmSpring.targetPosition = forearmTarget;
+		upperArm.spring = upperArmSpring;
+		forearm.spring = forearmSpring;
 	}
 }
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+	public string horizontalAxis;
+	public string verticalAxis;
+	private Vector3 initialPos;
+	
+	void Start () {
+		initialPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3 (Input.GetAxis (horizontalAxis), 0, Input.GetAxis (verticalAxis)) * .05f);
+	}
+}
