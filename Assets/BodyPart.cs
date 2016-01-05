db1e42363eaee75ab4d014d3f42f0e2f2a30795d
Tyler Bonnell <tbonnell@uw.edu>
2016-01-04 18:45:29 -0800

    added some sound effects

--- Assets/BodyPart.cs
+++ Assets/BodyPart.cs
@@ -5,11 +5,14 @@ public class BodyPart : MonoBehaviour {
 	
 	public PlayerMove player;
 	public float healthValue; // the value that the player loses when hit here
+	public AudioClip[] hitSounds;
 
 	void OnCollisionEnter (Collision collision) {
 		Arm a = collision.gameObject.GetComponent<Arm> ();
 		if (a != null && a.isPunching) {
 			player.Damage (healthValue);
+			if (hitSounds.Length > 0)
+				AudioSource.PlayClipAtPoint (hitSounds[Random.Range(0, hitSounds.Length)], transform.position);
 		}
 	}
 }
--- Assets/HeadScript.cs
+++ Assets/HeadScript.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class HeadScript : MonoBehaviour {
 
+	public AudioClip popSound;
+
 	public void PopHead () {
 		HingeJoint joint = GetComponent<HingeJoint> ();
 		joint.autoConfigureConnectedAnchor = false;
-		joint.connectedAnchor = joint.connectedAnchor + Vector3.up;
-		joint.anchor = joint.anchor - Vector3.up * 2f;
+		joint.connectedAnchor = joint.connectedAnchor + Vector3.up * 2f;
+		joint.anchor = joint.anchor - Vector3.up * 3f;
+		AudioSource.PlayClipAtPoint (popSound, transform.position, 2f);
 	}
 }
